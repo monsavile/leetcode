82e49bdbca21b462a5f1dc89949c31b2416c7b40
monsavile <monsavile@yandex.ru>
2025-03-15 20:29:48 +0000

    1. Two Sum

--- cmd/1_Two_Sum/main.go
+++ cmd/1_Two_Sum/main.go
@@ -1,17 +1,23 @@
 package main
 
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // []int{0, 1}
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))      // []int{1, 2}
+	fmt.Println(twoSum([]int{3, 3}, 6))         // []int{0, 1}
+}
+
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int, len(nums))
+	hash := make(map[int]int)
 
-	for i, num := range nums {
-		complement := target - num
-
-		if j, ok := m[complement]; ok {
-			return []int{i, j}
+	for i1, num := range nums {
+		if i2, ok := hash[target-num]; ok {
+			return []int{i2, i1}
 		}
 
-		m[num] = i
+		hash[num] = i1
 	}
 
-	return []int{}
+	return nil
 }
